e5399a57045c050e2cfa472836bca08cf1fbea5a
psalmuelle <bjoestores@gmail.com>
2022-12-14 22:26:21 +0100

    modified header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../../images/logo";
+import MenuIcon from '../../images/menu-icon.svg';
+import CloseIcon from "../../images/close-icon.svg";
 
 export default function Header() {
+  const [menu, setMenu] = useState(false)
+  const handleMenuClick =()=>{
+  return  setMenu(!menu)
+  }
   return (
-    <header className='w-full flex items-center justify-between py-3 px-8 sticky top-0 backdrop-blur-md text-[#333333]'>
-      <Logo className='block pointer' />
+    <header className='w-full flex items-center justify-between py-3 px-4 sm:px-10 md:px-8 sticky top-0 backdrop-blur-xl bg-white text-[#333333]'>
+      <Logo className='block pointer h-12 md:h-fit cursor-pointer' />
 
-      <ul className='flex justify-between items-center min-w-[400px] text- font-medium'>
+      <ul className='md:flex hidden justify-between items-center min-w-[400px] text- font-medium'>
         <li>
           <NavLink
             to='/'
@@ -15,6 +22,9 @@ export default function Header() {
                 padding: "2px",
                 color: isActive ? "#393F93" : "",
                 fontWeight: isActive ? "600" : "",
+                
+                border: isActive ? '2px solid #393f93': '',
+                borderRadius: '100% 50%'
               };
             }}>
             Tech Talents
@@ -28,6 +38,8 @@ export default function Header() {
                 padding: "2px",
                 color: isActive ? "#393F93" : "",
                 fontWeight: isActive ? "600" : "",
+                border: isActive ? '2px solid #393f93': '',
+                borderRadius: '100% 50%'
               };
             }}>
             Employer
@@ -41,6 +53,8 @@ export default function Header() {
                 padding: "2px",
                 color: isActive ? "#393F93" : "",
                 fontWeight: isActive ? "600" : "",
+                border: isActive ? '2px solid #393f93': '',
+                borderRadius: '100% 50%'
               };
             }}>
             About
@@ -54,15 +68,94 @@ export default function Header() {
                 padding: "2px",
                 color: isActive ? "#393F93" : "",
                 fontWeight: isActive ? "600" : "",
+                border: isActive ? '2px solid #393f93': '',
+                borderRadius: '100% 50%'
               };
             }}>
             Contact us
           </NavLink>
         </li>
       </ul>
-      <button className='border-none px-4 py-3 bg-[#393F93] text-white/95 rounded-lg font-medium'>
+      <button className='md:block hidden border-none px-4 py-3 bg-[#393F93] text-white/95 rounded-lg font-medium'>
         Apply For A Job
       </button>
+      <div className="md:hidden block w-7 h-7" onClick={handleMenuClick}>
+        <img src={menu? CloseIcon: MenuIcon} alt='open hamburger menu' className="block w-full h-full" />
+      </div>
+
+      {/* Mobile Menu */}
+    {
+      menu && (
+
+      <div className="fixed py-8 top-full w-full left-0 bg-white h-fit  flex flex-col items-center justify-center gap-4">
+      <ul className='md:hidden flex flex-col justify-between items-center gap-4 text- font-medium'>
+        <li>
+          <NavLink
+            to='/'
+            style={({ isActive }) => {
+              return {
+                padding: "2px",
+                color: isActive ? "#393F93" : "",
+                fontWeight: isActive ? "600" : "",
+                border: isActive ? '2px solid #393f93': '',
+                borderRadius: '100% 50%'
+              };
+            }}>
+            Tech Talents
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='employer'
+            style={({ isActive }) => {
+              return {
+                padding: "2px",
+                color: isActive ? "#393F93" : "",
+                fontWeight: isActive ? "600" : "",
+                border: isActive ? '2px solid #393f93': '',
+                borderRadius: '100% 50%'
+              };
+            }}>
+            Employer
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='about'
+            style={({ isActive }) => {
+              return {
+                padding: "2px",
+                color: isActive ? "#393F93" : "",
+                fontWeight: isActive ? "600" : "",
+                border: isActive ? '2px solid #393f93': '',
+                borderRadius: '100% 50%'
+              };
+            }}>
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to='contact-us'
+            style={({ isActive }) => {
+              return {
+                padding: "2px",
+                color: isActive ? "#393F93" : "",
+                fontWeight: isActive ? "600" : "",
+                border: isActive ? '2px solid #393f93': '',
+                borderRadius: '100% 50%'
+              };
+            }}>
+            Contact us
+          </NavLink>
+        </li>
+      </ul>
+      <button className='md:hidden block border-none px-4 py-3 bg-[#393F93] text-white/95 rounded-lg font-medium'>
+        Apply For A Job
+      </button>
+      </div>
+      )
+    }
     </header>
   );
 }
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,12 +1,17 @@
 import Header from "../../components/Header";
+import HeroImg from "../../images/hero.jpg";
 
 export default function LandingPage(){
     return (
-        <div className=" ">
+        <div className="">
             <Header/>
-            <main className="h-[22222px]">
-                This is an example of a landing page
+            <main className="flex">
+                <div>
+                    <h1 className="">This is going to be the Header of the Hero Section</h1>
+                </div>
+                <img src={HeroImg} alt="Hero section" className="block w-1/2" />
             </main>
+            <footer className="bg- bg-cyan-800 h-[2222px]"></footer>
         </div>
     )
 }
